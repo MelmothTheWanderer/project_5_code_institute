f102317e9b26560b26cc8abfb207cfb25db93ca9
Jamie Simms <jamiesimms@protonmail.com>
2024-06-11 14:36:59 +0100

    added querying in the searchbar by writing and wiring up our code in the products view and the urls

--- products/views.py
+++ products/views.py
@@ -1,20 +1,36 @@
-from django.shortcuts import render
 from .models import Product, Category
-from django.shortcuts import get_object_or_404
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.urls import reverse
 # Create your views here.
+
 
 def all_products(request):
     """
     A view that shows all of the products, with sorting and search queries
     """
     products = Product.objects.all()
+    query = None
 
-    context = { 
-        'products' : products
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    context = {
+        'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
+
 
 def product_detail(request, product_id):
     """
@@ -22,9 +38,8 @@ def product_detail(request, product_id):
     """
     product = get_object_or_404(Product, pk=product_id)
 
-    context = { 
-        'product' : product
+    context = {
+        'product': product
     }
 
     return render(request, 'products/product_details.html', context)
-
