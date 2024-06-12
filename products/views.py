3779af2abf7297c63361bf28f90156bad68e3e44
Jamie Simms <jamiesimms@protonmail.com>
2024-06-12 12:16:29 +0100

    added the ability to shop by catogories by using the links in the dropdown by adding that in views.py in the products app

--- products/views.py
+++ products/views.py
@@ -12,10 +12,17 @@ def all_products(request):
     """
     products = Product.objects.all()
     query = None
+    categories= None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
+            # Incase an empty search was submitted:
             if not query:
                 messages.error(
                     request, "You didn't enter any search criteria!")
@@ -27,6 +34,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories' : categories,
     }
 
     return render(request, 'products/products.html', context)
